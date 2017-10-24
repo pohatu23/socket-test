e6d05300550080f4f91cd528b6d6e68af0ed06ad
pohatu23 <33024755+pohatu23@users.noreply.github.com>
2017-10-24 17:57:05 +0200

    Update server.py

--- server.py
+++ server.py
@@ -1,12 +1,62 @@
 import socket
+from time import time, sleep
 
-hote = ''
-port = 12800
+GRAPHIC_DISPLAY = False
 
-connexion_principale = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+def disp(*args):
+    if GRAPHIC_DISPLAY:
+        print(**args)
 
-connexion_principale.bind((hote, port))
-connexion_principale.listen(5)
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
-connexion_avec_client, infos_connexion = connexion_principale.accept()
+host = ""
+port = 55556
+
+s.bind((host,port))
+s.listen(5)
+
+s.settimeout(0.0)
+
+
+clients = []
+
+t = time()
+
+while time()-t<20.0:
+
+    try:
+        co,inf = s.accept()
+        i = len(clients)
+        clients.append((co,inf,i))
+        disp("received client",i,":",co,inf)
+    except socket.error:
+        pass
+
+disp("Discussion is open\n")
+        
+
+def receive():
+    t = time()
+    
+    while time()-t < 600.0:
+        for client in clients:
+            try:
+                msg = client[0].recv(2048)
+                print(msg.decode())
+                if msg.decode() == "fin":
+                    return
+                i = client[2]
+
+                for client in clients:
+                    if client[2] != i:
+                        client[0].send(str(inf).encode()+b" : "+msg)
+                    
+            except socket.error:
+                sleep(0.1)
+
+receive()
+
+s.close()
+
 
