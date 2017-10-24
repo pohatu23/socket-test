35055c828c615d1809cd24b59e28263f9f8bb5e9
ogut197 <31800671+ogut197@users.noreply.github.com>
2017-10-24 15:37:06 +0200

    Update client.py

--- client.py
+++ client.py
@@ -1,10 +1,24 @@
 import socket
 
+socket.settimeout(0)
+
 hote = ""
 port = 55556
 
 connexion_avec_serveur = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 connexion_avec_serveur.connect((hote, port))
 
+print("=============== Bonjour ! ===============")
+
 #connexion_avec_serveur.send(b"coucou")
 #connexion_avec_serveur.recv(1024)
+
+def recv():
+  try:
+    msg = connexion_avec_serveur.recv(1024)
+    print(msg)
+  except socket.error:
+    pass
+
+def send(message):
+  connexion_avec_serveur.send(message)
